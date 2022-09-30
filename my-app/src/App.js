73d04e7cbee5bb55bb042975a7e02ddd75c10b33
Sunilrath101 <sunilrath.ssr@gmail.com>
2022-09-30 22:27:09 +0530

    day-4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import Men from "./men.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Footer />
+      <Men />
     </div>
   );
 }
---
+++ my-app/src/men.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const men = () => {
+  return <div>Men Wear</div>;
+};
+
+export default men;
